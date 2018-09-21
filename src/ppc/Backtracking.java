3437f878f1dcd7403b5726366224d6c7ba5a0892
ValentinLe <valou.leblond1@gmail.com>
2018-09-21 15:02:47 +0200

    supressions variables inutiles

--- src/ppc/Backtracking.java
+++ src/ppc/Backtracking.java
@@ -10,15 +10,15 @@ import java.util.Set;
 import representations.*;
 
 public class Backtracking {
-    
+
     private Set<Variable> variables;
     private Set<Constraint> constraints;
-    
+
     public Backtracking(Set<Variable> variables, Set<Constraint> constraints){
         this.variables = variables;
         this.constraints = constraints;
     }
-    
+
     public boolean isComplete(Map<Variable, String> voiture) {
         for(Variable var : this.variables) {
             if (!voiture.containsKey(var)) {
@@ -27,7 +27,7 @@ public class Backtracking {
         }
         return true;
     }
-    
+
     public int heuristic(Variable var) {
         int cpt = 0;
         for(Constraint c : this.constraints) {
@@ -39,7 +39,7 @@ public class Backtracking {
         }
         return cpt;
     }
-    
+
     public ArrayList<Variable> getSortVariable() {
         ArrayList<Variable> listVar = new ArrayList<>();
         Iterator<Variable> iter = this.variables.iterator();
@@ -53,17 +53,13 @@ public class Backtracking {
         Collections.reverse(listVar);
         return listVar;
     }
-    
+
     public Variable choiceVar(ArrayList<Variable> sortVar) {
         Variable var = sortVar.get(0);
         sortVar.remove(0);
         return var;
     }
-    
-    public Map<Variable, String> solution() {
-        return backtracking(new HashMap<>(), getSortVariable());
-    }
-    
+
     public boolean isCompatible(Variable var, String value, Map<Variable, String> voiture) {
         voiture.put(var,value);
         for(Constraint c : this.constraints) {
@@ -73,13 +69,16 @@ public class Backtracking {
         }
         return true;
     }
-    
+
+    public Map<Variable, String> solution() {
+      return backtracking(new HashMap<>(), getSortVariable());
+    }
+
     public Map<Variable, String> backtracking(Map<Variable, String> voiture, ArrayList<Variable> sortVar) {
         if (this.isComplete(voiture)) {
             return voiture;
         }
         Variable var = choiceVar(sortVar);
-        Set<String> csp;
         Map<Variable, String> newVoiture;
         for(String value : var.getDomaine()) {
             if (this.isCompatible(var, value, voiture)) {
