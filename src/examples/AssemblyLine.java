aa3007363c6de4c95ada95c27e426283c911de72
ValentinLe <valou.leblond1@gmail.com>
2018-10-09 20:45:09 +0200

    random gen assambly, deb dfs/bfs

--- src/examples/AssemblyLine.java
+++ src/examples/AssemblyLine.java
@@ -2,58 +2,84 @@
 package examples;
 
 import java.util.*;
-import planning.Action;
-import representations.Variable;
+import planning.*;
+import representations.*;
 
 public class AssemblyLine {
     
     private Map<Variable, String> voiture;
     private Map<String, Variable> mapVar;
+    private Set<String> domainColors;
+    private Set<String> elementsBool;
+    private Set<String> componentsColor;
+    private Set<Variable> HAS_ELEMENT;
+    private Set<Variable> X_Y_WHEEL_COLOR;
     
     public AssemblyLine() {
-       Set<String> varBool = new HashSet<>(Arrays.asList(
+       this.elementsBool = new HashSet<>(Arrays.asList(
                "HAS_CHASSIS", "HAS_FRONT_LEFT_WHEEL", "HAS_FRONT_RIGHT_WHEEL",
                "HAS_REAR_LEFT_WHEEL", "HAS_REAR_RIGHT_WHEEL", "HAS_BODY"
        ));
        
-       Set<String> booleans = new HashSet<>(Arrays.asList(
+       Set<String> domainBooleans = new HashSet<>(Arrays.asList(
                "TRUE", "FALSE"
        ));
        
-       Set<String> components = new HashSet<>(Arrays.asList(
+       this.componentsColor = new HashSet<>(Arrays.asList(
                "FRONT_COLOR", "LEFT_COLOR", "REAR_COLOR", "RIGHT_COLOR", "ROOF_COLOR"
        ));
        
-       Set<String> colors = new HashSet<>(Arrays.asList(
+       this.domainColors = new HashSet<>(Arrays.asList(
                "GRAY", "BLACK", "WHITE", "RED", "GREEN", "BLUE", "ORANGE", "YELLOW"
        ));
        
-       Set<Variable> HAS_ELEMENT = new HashSet<>();
+       this.HAS_ELEMENT = new HashSet<>();
        
        this.mapVar = new HashMap<>();
        
        this.voiture = new HashMap<>();
        
-       for (String varBoolean : varBool) {
-           Variable var = new Variable(varBoolean, booleans);
-           HAS_ELEMENT.add(new Variable(varBoolean, booleans));
+       for (String varBoolean : this.elementsBool) {
+           Variable var = new Variable(varBoolean, domainBooleans);
+           this.HAS_ELEMENT.add(new Variable(varBoolean, domainBooleans));
            this.voiture.put(var,"FALSE");
            this.mapVar.put(varBoolean, var);
        }
        
-       Set<Variable> X_Y_WHEEL_COLOR = new HashSet<>();
+       this.X_Y_WHEEL_COLOR = new HashSet<>();
        
-       for (String component : components) {
-           Variable var = new Variable(component, colors);
-           X_Y_WHEEL_COLOR.add(var);
-           this.voiture.put(var,"BLACK");
+       for (String component : this.componentsColor) {
+           Variable var = new Variable(component, this.domainColors);
+           this.X_Y_WHEEL_COLOR.add(var);
+           this.voiture.put(var,"GRAY");
            this.mapVar.put(component, var);
        }
        
-        
+       voiture = generateCar();
+        System.out.println(voiture);
     }
     
-    public Action getAction1() {
-        Map<Variable, String> premisse = new HashMap<>();
+    public String choiceValue(Set<String> setValues) {
+        Random r = new Random();
+        int valueRandom = r.nextInt(setValues.size());
+        int i = 0;
+        for (String value : setValues) {
+            if (i++ == valueRandom) {
+                return value;
+            }
+        }
+        return null;
+    }
+    
+    public Map<Variable, String> generateCar() {
+        Map<Variable, String> voiture = new HashMap<>(this.voiture);
+        String colorSelect = choiceValue(this.domainColors);
+        for (Variable var : this.HAS_ELEMENT) {
+            voiture.put(var, "TRUE");
+        }
+        for (Variable var : this.X_Y_WHEEL_COLOR) {
+            voiture.put(var, colorSelect);
+        }
+        return voiture;
     }
 }
--- src/planning/PlanningProblem.java
+++ src/planning/PlanningProblem.java
@@ -1,6 +1,60 @@
 
 package planning;
 
+import java.util.*;
+import representations.*;
+
 public class PlanningProblem {
     
+    private State initialState;
+    private State goal;
+    private Set<Action> actions;
+    
+    public PlanningProblem(State initialState, State goal, Set<Action> actions) {
+        this.initialState = initialState;
+        this.goal = goal;
+        this.actions = actions;
+    }
+    
+    public Stack<Action> dfs(State state, Stack<Action> plan, 
+            Set<State> closed) {
+        
+        if (state.satisfies(this.goal.getVoiture())) {
+            return plan;
+        } else {
+            for (Action action : this.actions) {
+                if (action.isApplicable(state)) {
+                    State next = action.apply(state);
+                    if (!closed.contains(next)) {
+                        plan.push(action);
+                        closed.add(next);
+                        Stack<Action> subplan = dfs(next, plan, closed);
+                        if (!(subplan != null)) {
+                            return subplan;
+                        } else {
+                            plan.pop();
+                        }
+                    }
+                }
+            }
+            return plan;
+        }
+    }
+    
+    public void dfsIter() {
+        
+    }
+    
+    public Queue<State> bfs() {
+        Map<State, State> father = new HashMap<>();
+        Map<State, Action> plan = new HashMap<>();
+        Set<State> closed = new HashSet<>();
+        Queue<State> open = new LinkedList<>();
+        open.add(this.initialState);
+        father.put(this.initialState, null);
+        while (!open.isEmpty()) {
+            
+        }
+        return null;
+    }
 }
