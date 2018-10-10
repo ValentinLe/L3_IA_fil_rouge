cb827b4fc699173bcd96e3dee60d010bf9ffee86
Valentin Leblond <21609038@campus.unicaen.fr>
2018-10-10 17:33:17 +0200

    planningProblem

--- src/planning/PlanningProblem.java
+++ src/planning/PlanningProblem.java
@@ -45,7 +45,7 @@ public class PlanningProblem {
         
     }
     
-    public Queue<State> bfs() {
+    public Queue<Action> bfs() {
         Map<State, State> father = new HashMap<>();
         Map<State, Action> plan = new HashMap<>();
         Set<State> closed = new HashSet<>();
@@ -53,8 +53,35 @@ public class PlanningProblem {
         open.add(this.initialState);
         father.put(this.initialState, null);
         while (!open.isEmpty()) {
-            
+            State state = open.remove();
+            closed.add(state);
+            for (Action action : this.actions) {
+                if (action.isApplicable(state)) {
+                    State next = action.apply(state);
+                    if (!closed.contains(next) && !open.contains(next)) {
+                        father.put(next, state);
+                        plan.put(next, action);
+                        if (next.satisfies(this.goal.getVoiture())) {
+                            return this.getBfsPlan(father, plan, next);
+                        } else {
+                            open.add(next);
+                        }
+                    }
+                }
+            }
         }
         return null;
+    }
+    
+    public Queue<Action> getBfsPlan(Map<State, State> father, 
+            Map<State, Action> actions, State goal) {
+        
+        Queue<Action> plan = new LinkedList<>();
+        while (goal != null) {
+            plan.add(actions.get(goal));
+            goal = father.get(goal);
+        }
+        Collections.reverse((LinkedList<Action>)plan);
+        return plan;
     }
 }
