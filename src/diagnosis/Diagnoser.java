ad3ca60bc7849263eba10fd96f19c504362526e2
ValentinLe <valou.leblond1@gmail.com>
2018-11-19 18:04:25 +0100

    debut doc dignoser

--- src/diagnosis/Diagnoser.java
+++ src/diagnosis/Diagnoser.java
@@ -5,41 +5,78 @@ import java.util.*;
 import representations.*;
 import ppc.*;
 
+/**
+ * Cette classe permet de repérérer une explication de non satisfactions d'un
+ * groupe de contraintes
+ */
 public class Diagnoser {
 
     private Backtracking backtrack;
     private Map<Variable, Set<String>> variablesWithDomain;
 
+    /**
+     * Build an instance of Diagnoser
+     * @param backtrack a instance of backtrack pour trouver des solutions a un
+     * etat de voiture donnee, elle contiendra l'ensemble des contraintes et des
+     * variables
+     */
     public Diagnoser(Backtracking backtrack) {
         this.backtrack = backtrack;
+        // utilisation de la fonction transformToMap qui permet de recuperer
+        // une map des variables ainsi qu'une copie de leur domaine qu'on pourra
+        // reduire
         this.variablesWithDomain = this.backtrack.transformToMap(this.backtrack.getVariables());
     }
 
-    public void add(Variable variable, String value) {
+    /**
+     * Ajoute une variable dans la map des variable avec leur domaine copié et
+     * l'ajoute également dans l'ensemble de variable de l'instance de Backtracking
+     * @param variable la variable à ajouter
+     */
+    public void add(Variable variable) {
         this.backtrack.addVariable(variable);
         this.variablesWithDomain.put(variable, new HashSet<>(variable.getDomaine()));
     }
-
+    
+    /**
+     * Supprime une variable dans la map des variables et dans l'ensemble de 
+     * variable du backtrack
+     * @param variable la variable a supprimée 
+     */
     public void remove(Variable variable) {
         this.backtrack.removeVariable(variable);
         this.variablesWithDomain.remove(variable);
     }
 
+    /**
+     * Test si un ensemble de choix donnée est une explication avec l'ajout d'un 
+     * choix supplémentaire. Les choix sont une explication si il n'existe pas 
+     * de solution qui permet de satisfaire toutes les contraintes
+     * @param choices l'ensemble de choix
+     * @param variable la variable du choix supplémentaire
+     * @param value la valeur de la variable du choix supplémentaire
+     * @return true si le choix supplémentaire avec les autre choix ne 
+     */
     public boolean isExplanation(Map<Variable, String> choices, Variable variable, String value) {
         Set<String> reduceDomain;
         Map<Variable, Set<String>> copyDomain = new HashMap<>(this.variablesWithDomain);
         for (Variable var : choices.keySet()) {
+            // for all choice in the set of choices
+            // reduce the domain too the value assigned in the choices
             reduceDomain = new HashSet<>();
             reduceDomain.add(choices.get(var));
             copyDomain.put(var, reduceDomain);
         }
+        // add the new choix with the domain reduce to their value
         reduceDomain = new HashSet<>();
         reduceDomain.add(value);
         copyDomain.put(variable, reduceDomain);
+        // try to find a solution
         Map<Variable, String> solution = this.backtrack.backtrackingFilter(choices, copyDomain);
+        // solution = null <=> no solution found
         return solution == null;
     }
-
+    
     public Map<Variable, String> findMinimalInclusionExplanation(Map<Variable, String> choices, Variable variable, String value) {
         Map<Variable, String> explanation = new HashMap<>(choices);
         Map<Variable, String> ePrime;
